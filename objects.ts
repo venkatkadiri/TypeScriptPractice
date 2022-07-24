25d7e326de0758ecab1d5faebb955105cf3a1448
Venkat kadiri <venkatkadiri@hotmail.com>
2022-07-24 15:51:19 +0530

    till union types

--- objects.ts
+++ objects.ts
@@ -8,11 +8,11 @@ const person ={
 objTest(person);
 
 // return type of a given function to be an object
-type applicant = {
+type Applicant = {
     name:string,
     age:number
 }
-function objTest2(name:string,age:number):applicant{
+function objTest2(name:string,age:number):Applicant{
 const person2 ={
     name,
     age
@@ -22,3 +22,23 @@ return person2;
 // declaring object type during object initialization
 const val:{name:string,age:number} ={name:'venkat',age:25}; 
 console.log(objTest2(val.name,val.age));
+
+type Address = {
+name:string,
+age:number,
+address:{
+    street:string,
+    lane?:string,
+}
+}
+function getAddress(name:string ='venkat',age:number = 25,street:string ='45-464/2/1',lane:string ='malkajgiri'):Address{
+return {
+    name,
+    age,
+    address:{
+        street,
+        lane
+    }
+}
+}
+console.log(getAddress());
